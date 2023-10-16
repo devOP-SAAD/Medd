c092abe363440318e959cb2c440b9f65d99233c4
devOP-SAAD <147581442+devOP-SAAD@users.noreply.github.com>
2023-10-15 23:14:06 -0700

    adA

--- visits/src/Login.js
+++ visits/src/Login.js
@@ -1,39 +1,56 @@
 import React, { useState } from 'react';
-import { Container, Grid, Typography, TextField, Button, Link } from '@mui/material';
-import axios from 'axios';
+import { Grid, Typography, TextField, Button, Link } from '@mui/material';
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [loginError, setLoginError] = useState('');
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+  const [registeredUsers, setRegisteredUsers] = useState([]);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.get('https://mocki.io/v1/702b020c-b344-4a78-a7ac-62ccbd9e47f0');
-      const users = response.data;
-
-      const user = users.find((user) => user.username === username && user.password === password);
-
-      if (user) {
-        // Authentication successful
-        // You can save the user's information (e.g., user ID) to local storage or global state.
-        localStorage.setItem('userId', user.id);
-        // Redirect to the dashboard or another page upon successful login.
-        // Replace '/dashboard' with your desired route.
-        alert('Success')
-        // Clear the input fields
-        setUsername('');
-        setPassword('');
-      } else {
-        setLoginError('Invalid username or password. Please try again.');
-      }
-    } catch (error) {
-      setLoginError('An error occurred while attempting to log in. Please try again.');
-    }
+  const toggleSignUp = () => {
+    setIsSignUp(!isSignUp);
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSignUp) {
+      // Handle signup logic by adding the formData to the list of registered users.
+      setRegisteredUsers([...registeredUsers, formData]);
+      console.log('Signup data:', formData);
+      alert('Signup successful. Please login.');
+
+      // Reset the form fields
+      setFormData({
+        fullName: '',
+        email: '',
+        password: '',
+      });
+    } else {
+      // Handle login logic by checking if the entered data matches any registered user.
+      const matchingUser = registeredUsers.find(
+        (user) => user.email === formData.email && user.password === formData.password
+      );
+      if (matchingUser) {
+        console.log('Login data:', formData);
+        alert('Login successful.');
+      } else {
+        console.log('Login data:', formData);
+        alert('Incorrect email or password. Please try again.');
+      }
+    }
+  };
 
   const centerStyle = {
     display: 'flex',
@@ -42,60 +59,61 @@ function LoginPage() {
     alignItems: 'center',
     height: '100vh',
   };
+
   return (
-    <Grid container xs={12} lg={12}  style={{ height: '650px' }}>
-     
-    <Grid   xs={12} lg={6} style={{background:'#01619B'}}>
-      
-    <Grid container style={centerStyle}>
-      <Typography  >
-        <span style={{ fontSize: '30px', color: 'white', fontFamily: 'serif', fontWeight: 'bold' }}>Welcome to the <br />MEDIREMOTE</span><br /><br />
-        <span style={{ color: 'white' }}>A Brand Of E-HealthCare System and <br /> Wireless Communications. Current and <br />
-          Future Challenges</span><br />
-        <span style={{ color: 'white' }}>copyright @ 2023 MEDIREMOTE. All Rights Reserved</span>
-      </Typography>
-    </Grid>
-
-    </Grid>
-
-
-
+    <Grid container xs={12} lg={12} justify="center" alignItems="center" style={{ height: '650px' }}>
       <Grid xs={12} md={6} lg={6}>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '87vh',margin:'10px' }}>
-          <form sx={{ display: 'flex', flexDirection: 'column', padding: '10px' }} onSubmit={handleLogin}>
-          <Grid style={{display:'flex', justifyContent:'center' , paddingBottom:'50px'}}>
-          <img src={process.env.PUBLIC_URL + '/1.png'} alt='MediRemote' style={{  marginRight: '10px', width:'300px' ,  height:'100%' }} />
-          </Grid>
+        <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '10px' }} onSubmit={handleSubmit}>
           <TextField
-              label="Username"
-              fullWidth
-              required
-              style={{ margin: '10px' }}
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+            label={isSignUp ? "Full Name" : "Email"}
+            required
+            name={isSignUp ? "fullName" : "email"}
+            value={formData[isSignUp ? "fullName" : "email"]}
+            onChange={handleChange}
+            style={{ width: '100%', maxWidth: '400px', margin: '10px' }}
+          />
+          {isSignUp && (
             <TextField
-              type="password"
-              label="Password"
-              fullWidth
+              label="Email"
               required
-              style={{ margin: '10px' }}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              style={{ width: '100%', maxWidth: '400px', margin: '10px' }}
             />
-            <div sx={{ marginTop: 2 }}>
-              <Button type="submit" variant="contained" color="primary" fullWidth style={{ margin: '10px',borderRadius:'30px' }}>
-                Login
-              </Button>
-            </div>
-            <div style={{ display:'flex' ,  justifyContent:'center'}}>
-            {loginError && <Typography  color="error">{loginError}</Typography>}
-            </div>
-          </form>
-          
-       
-        </div>
-        
+          )}
+          <TextField
+            type="password"
+            label={isSignUp ? "Create Password" : "Password"}
+            required
+            name={isSignUp ? "password" : "password"}
+            value={formData.password}
+            onChange={handleChange}
+            style={{ width: '100%', maxWidth: '400px', margin: '10px' }}
+          />
+          <div style={{ marginTop: '20px', width: '100%', maxWidth: '400px' }}>
+            <Button type="submit" variant="contained" color="primary" style={{ width: '100%', borderRadius: '30px' }}>
+              {isSignUp ? "Sign up" : "Login"}
+            </Button>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+            {isSignUp ? (
+              <Link onClick={toggleSignUp} style={{ color: 'blue' }}>Back to Login</Link>
+            ) : (
+              <Link onClick={toggleSignUp} style={{ color: 'blue' }}>Sign up</Link>
+            )}
+          </div>
+        </form>
+      </Grid>
+      <Grid xs={12} lg={6} style={{ background: '#01619B' }}>
+        <Grid container style={centerStyle}>
+          <Typography>
+            <span style={{ fontSize: '30px', color: 'white', fontFamily: 'serif', fontWeight: 'bold' }}>Welcome to the <br />MEDIREMOTE</span><br /><br />
+            <span style={{ color: 'white' }}>A Brand Of E-HealthCare System and <br /> Wireless Communications. Current and <br />
+              Future Challenges</span><br />
+            <span style={{ color: 'white' }}>copyright @ 2023 MEDIREMOTE. All Rights Reserved</span>
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
   );
